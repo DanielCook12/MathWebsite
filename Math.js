14c0c53847ec4e3d8f7e0c672728c1998c8cf473
Torca7 <72940254+Torca7@users.noreply.github.com>
2020-10-16 08:35:58 -0400

    Update Math.js

--- Math.js
+++ Math.js
@@ -1,13 +1,28 @@
 c = document.getElementById("canvas");
 d = c.getContext("2d");
-d.font ="30px Arial";
-leftText = "Place Left Math Here"
-rightText = "Place Right Math Here"
+var shapes = [0];
+var shapesX = [10];
+var shapesY = [10];
+var shapesVY = [2];
+var shapesVX = [0];
+var shapesH = [50];
+var shapesW = [50];
+var selected = -1;
+var mx = 0;
+var my = 0;
 
-render();
+setInterval(function() {
+	render();
+	update();
+}, 33);
+
 function render() {
+	//clear screen
+	d.clearRect(0, 0, 1000, 700);
+	
 	// Render Scale
 	d.fillStyle = "#000000";
+	
 	// Main Triangle
 	d.beginPath();
 	d.moveTo(500, 250);
@@ -15,20 +30,65 @@ function render() {
 	d.lineTo(675, 450);
 	d.lineTo(500, 250);
 	d.fill();
+	
 	// Axel/Circle
 	d.beginPath();
 	d.arc(500, 250, 40, 0, Math.PI*2);
 	d.fill();
+	
 	// Main Board of balancing
 	d.fillRect(225, 242.5, 550, 15);
 	d.fillRect(225, 230, 20, 15);
 	d.fillRect(755, 230, 20, 15);
+	
 	//Left Scale
 	d.fillRect(40, 210, 400, 20);
-	// Left Text
-	d.fillText(leftText, 100, 200);
+	
 	//Right Scale
 	d.fillRect(560, 210, 400, 20);
-	// Right Text
-	d.fillText(rightText, 610, 200);
+	
+	//Render Shapes
+	for (var i = 0; i<shapes.length; i++) {
+		d.fillRect(shapesX[i], shapesY[i], 50, 50);
+	}
 }
+
+function update() {
+	// Move Shapes
+	for (var i = 0; i < shapes.length; i++) {
+		shapesX[i] += shapesVX[i];
+		shapesY[i] += shapesVY[i];
+		shapesVY[i] += 0.25;
+		if (shapesVX[i] != 0) {
+			shapesVX[i] -= 0.1;
+		}
+		if (shapesY[i] >= 210 - shapesH[i]) {
+			shapesVY[i] = 0;
+			shapesY[i] = 210 - shapesH[i];
+		}
+		if (selected == i) {
+			shapesX[i] = mx;
+			shapesY[i] = my;
+			console.log(mx);
+		}
+	}
+}
+
+document.addEventListener('mousemove', function(event) {
+	mx = event.offsetX;
+	my = event.offsetY;
+});
+
+document.addEventListener('click', function(event) {
+	mx = event.offsetX;
+	my = event.offsetY;
+	if (selected == -1) {
+		for (var i = 0; i < shapes.length; i++) {
+			if (mx - shapesX[i] <= shapesW[i] && mx - shapesX[i] >= 0 && my - shapesY[i] >= 0 && my - shapesY[i] <= shapesH[i]) {
+				selected = i;
+			}
+		}
+	} else {
+		selected = -1;
+	}
+});
