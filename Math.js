96db33a468407daf1b042df3382a4c08946ca24b
DanielCook12 <72940185+DanielCook12@users.noreply.github.com>
2020-10-16 08:09:02 -0400

    Add files via upload

--- Math.js
+++ Math.js
@@ -1,11 +1,13 @@
 c = document.getElementById("canvas");
 d = c.getContext("2d");
+d.font ="30px Arial";
+leftText = "Place Left Math Here"
+rightText = "Place Right Math Here"
 
 render();
 function render() {
 	// Render Scale
 	d.fillStyle = "#000000";
-	
 	// Main Triangle
 	d.beginPath();
 	d.moveTo(500, 250);
@@ -13,20 +15,20 @@ function render() {
 	d.lineTo(675, 450);
 	d.lineTo(500, 250);
 	d.fill();
-	
 	// Axel/Circle
 	d.beginPath();
 	d.arc(500, 250, 40, 0, Math.PI*2);
 	d.fill();
-	
 	// Main Board of balancing
 	d.fillRect(225, 242.5, 550, 15);
 	d.fillRect(225, 230, 20, 15);
 	d.fillRect(755, 230, 20, 15);
-	
 	//Left Scale
 	d.fillRect(40, 210, 400, 20);
-	
+	// Left Text
+	d.fillText(leftText, 100, 200);
 	//Right Scale
 	d.fillRect(560, 210, 400, 20);
+	// Right Text
+	d.fillText(rightText, 610, 200);
 }
